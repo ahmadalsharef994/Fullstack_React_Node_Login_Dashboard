1eafd80b8cb6ffc2e943b761966912c2eb2fbfa6
ahmadalsharef994 <51163921+ahmadalsharef994@users.noreply.github.com>
2022-02-27 00:10:34 +0530

    Update Dashboard.js

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,87 +1,85 @@
-
 import {useState, useEffect} from "react";
 import jwt_decode from "jwt-decode";
 import { Table, Badge, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { MdForwardToInbox } from "react-icons/md";
+
 
 function Dashboard() {
 
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
-  const [content, setContent] = useState("");
-  const [messages, setMessages]=useState([])
+	  const [from, setFrom] = useState("");
+	  const [to, setTo] = useState("");
+	  const [content, setContent] = useState("");
+	  const [messages, setMessages]=useState([])
 
-  useEffect(() => { //get jwt token
-    var token = localStorage.getItem('token')
-    var decoded = jwt_decode(token);
-    setFrom(decoded.email)
-    console.log(
-      "This only happens ONCE.  But it happens AFTER the initial render."
-    );
-  }, []);
+	  useEffect(() => { //get jwt token
+	    var token = localStorage.getItem('token')
+	    var decoded = jwt_decode(token);
+	    setFrom(decoded.email)
+	    console.log(
+	      "This only happens ONCE.  But it happens AFTER the initial render."
+	    );
+	  }, []);
 
 
+	async function  sendMessage(event){
+	    event.preventDefault(); //prevent default form redirect behavior
+	    const response = await fetch("http://localhost:1337/sendMessage", {
+		method: 'POST',
+		headers: {
+		    "content-type": "application/json"            
+		},
+		body: JSON.stringify({
+			from,
+			to,
+			content
+		    })
+	    })
+	    const data = await response.json()
+	    console.log(data)
+		}
+	
+	async function  forwardMessage(originalWriter, content){
+		const to = prompt('Please enter destination address')
+		content+=`\n______________\n Forwarded \n Original: ${originalWriter} -----> ${to}`
+		const response = await fetch("http://localhost:1337/sendMessage", {
+		    method: 'POST',
+		    headers: {
+			"content-type": "application/json"
+		    },
+		    body: JSON.stringify({
+			    from,
+			    to,
+			    content
+			})
+		})
+		const data = await response.json()
+		console.log(data)
+		}
 
-async function  sendMessage(event){
-    event.preventDefault(); //prevent default form redirect behavior
-    const response = await fetch("http://localhost:1337/sendMessage", {
-        method: 'POST',
-        headers: {
-            "content-type": "application/json"
-            
-        },
-        body: JSON.stringify({
-                from,
-                to,
-                content
-            })
-    })
-
-    const data = await response.json()
-
-    console.log(data)
-
-	}
-async function  forwardMessage(originalWriter, content){
-
-        const to = prompt('Please enter destination address')
-        content+=`\n______________\n Forwarded \n Original: ${originalWriter} -----> ${to}`
-        const response = await fetch("http://localhost:1337/sendMessage", {
-            method: 'POST',
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                    from,
-                    to,
-                    content
-                })
-        })
-        const data = await response.json()
-        console.log(data)
-        }
-async function  checkInbox(event){
-        const response = await fetch("http://localhost:1337/checkInbox", {
-            method: 'POST',
-            headers: {
-                "content-type": "application/json"      
-            },
-            body: JSON.stringify({
-                from
-            })
-        })
-        const data = await response.json()
-        setMessages(data)
-        }
+	async function  checkInbox(event){
+		const response = await fetch("http://localhost:1337/checkInbox", {
+		    method: 'POST',
+		    headers: {
+			"content-type": "application/json"      
+		    },
+		    body: JSON.stringify({
+			from
+		    })
+		})
+		const data = await response.json()
+		console.log(data)
+		setMessages(data)
+		}
 
 
   return (
+	  
     <div>
+	  
       <h1 style={{textAlign: 'center'}}><Badge bg="success">Dashboard</Badge></h1>
       <h6 style={{textAlign: 'center'}}><Badge  bg="primary">Logged in as {from}</Badge></h6>
-    <br />
+      <br />
 
     <form className="form-inline" onSubmit={sendMessage}>
         <h3>Compose Message</h3>
@@ -96,12 +94,14 @@ async function  checkInbox(event){
     </form>
 
     <br/>
+		
     <div>
         <h2>List Messages</h2>
         <br />
         <Button variant="primary" size="sm" onClick={checkInbox}> Fetch from Inbox</Button>
         <br />       
         <br />    
+		
         <Table striped bordered hover>
         <thead>
             <tr>
